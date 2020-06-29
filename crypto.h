3a4bb2b0d49dd0f1a2a4fc076938453e426c79c8
Marco Pettorali <marcopettoral1@gmail.com>
2020-06-29 12:10:48 +0200

    removed "shared_key" variable from crypto.h

--- crypto.h
+++ crypto.h
@@ -6,9 +6,6 @@
 #define GCM_KEY_SIZE 16
 #define GCM_TAG_SIZE 16
 
-// I'm not sure to leave it here
-unsigned char* shared_key;
-
 int gcm_encrypt(unsigned char* plaintext, int plaintext_len, unsigned char* aad, int aad_len, unsigned char* key, unsigned char* iv, int iv_len, unsigned char* ciphertext, unsigned char* tag);
 
 int gcm_decrypt(unsigned char* ciphertext, int ciphertext_len, unsigned char* aad, int aad_len, unsigned char* tag, unsigned char* key, unsigned char* iv, int iv_len, unsigned char* plaintext);
